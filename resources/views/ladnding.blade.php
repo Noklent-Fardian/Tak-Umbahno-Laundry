7331865a44ef652d41b9bf2d06d5aee2041e8341
Noklent <noklen350@gmail.com>
2024-05-06 15:38:10 +0700

    add  tentang kami

--- resources/views/ladnding.blade.php
+++ resources/views/ladnding.blade.php
@@ -51,12 +51,12 @@
 
             <nav id="navmenu" class="navmenu">
                 <ul>
-                    <li><a href="#hero" class="">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#services">Services</a></li>
+                    <li><a href="#hero" class="">Beranda</a></li>
+                    <li><a href="#about">Tentang Kami</a></li>
+                    <li><a href="#services">Kayanan</a></li>
                     <li><a href="#team">Team</a></li>
-                    <li><a href="#pricing">Pricing</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#pricing">Harga</a></li>
+                    <li><a href="#contact">Hubungi Kami</a></li>
                 </ul>
                 <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
             </nav>
@@ -75,10 +75,10 @@
                 <div class="row gy-4">
                     <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
                         <h1 class="">Solusi Cerdas Pakaian Kotor Anda</h1>
-                        <p class="">Anda Malas, Kami Senang</p>
+                        <p class="">Anda Malas, Kami Senang✋😊 </p>
                         <div class="d-flex">
                             <a href="#about" class="btn-get-started">Log-in</a>
-                            <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+                            <a href="https://youtu.be/MMRcbv0PNbo"
                                 class="glightbox btn-watch-video d-flex align-items-center"><i
                                     class="bi bi-play-circle"></i><span>Watch Video</span></a>
                         </div>
@@ -143,13 +143,13 @@
                                 alt=""></div>
                         <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid"
                                 alt=""></div>
-                        <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid"
+                        <div class="swiper-slide"><img src="assets/img/clients/client-5.svg" class="img-fluid"
                                 alt=""></div>
                         <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid"
                                 alt=""></div>
-                        <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid"
+                        <div class="swiper-slide"><img src="assets/img/clients/client-7.svg" class="img-fluid"
                                 alt=""></div>
-                        <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid"
+                        <div class="swiper-slide"><img src="assets/img/clients/client-8.jpg" class="img-fluid"
                                 alt=""></div>
                     </div>
                 </div>
@@ -163,7 +163,7 @@
 
             <!-- Section Title -->
             <div class="container section-title" data-aos="fade-up">
-                <h2 class="">About Us</h2>
+                <h2 class="">Tentang Kami</h2>
             </div><!-- End Section Title -->
 
             <div class="container">
@@ -172,25 +172,24 @@
 
                     <div class="col-lg-6 content" data-aos="fade-up" data-aos-delay="100">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore
-                            magna aliqua.
+                            Taumbahno Laundry hadir sebagai solusi praktis untuk kebutuhan mencuci pakaian Anda. Kami
+                            menawarkan berbagai layanan untuk memenuhi kebutuhan Anda.
                         </p>
                         <ul>
-                            <li><i class="bi bi-check2-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat.</span></li>
-                            <li><i class="bi bi-check2-circle"></i> <span>Duis aute irure dolor in reprehenderit in
-                                    voluptate velit.</span></li>
-                            <li><i class="bi bi-check2-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea
-                                    commodo</span></li>
+                            <li><i class="bi bi-check2-circle"></i> <span> Cuci Kering Express: cocok untuk jasa
+                                    laundry cepat, biasanya selesai dalam waktu 12 jam atau kurang.</span></li>
+                            <li><i class="bi bi-check2-circle"></i> <span> Cuci Reguler :Layanan ini merupakan layanan
+                                    standar untuk mencuci pakaian sehari-hari.</span></li>
+                            <li><i class="bi bi-check2-circle"></i> <span> Cuci Satuan : Layanan ini khusus untuk
+                                    mencuci item-item tertentu, seperti jas, sepatu, selimut, tas, dan boneka</span>
+                            </li>
                         </ul>
                     </div>
 
                     <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-                            laborum. </p>
+                        <p>Taumbahno Laundry siap membantu Anda dengan berbagai kebutuhan laundry Anda. Kami menawarkan
+                            layanan yang berkualitas dengan harga yang kompetitif. Kunjungi laundry kami atau hubungi
+                            kami melalui website atau media sosial untuk informasi lebih lanjut. </p>
                         <a href="#" class="read-more"><span>Read More</span><i
                                 class="bi bi-arrow-right"></i></a>
                     </div>
@@ -201,155 +200,20 @@
 
         </section><!-- /About Section -->
 
-        <!-- Why Us Section -->
-        <section id="why-us" class="section why-us" data-builder="section">
-
-            <div class="container-fluid">
-
-                <div class="row gy-4">
-
-                    <div class="col-lg-7 d-flex flex-column justify-content-center order-2 order-lg-1">
-
-                        <div class="content px-xl-5" data-aos="fade-up" data-aos-delay="100">
-                            <h3 class=""><span class="">Eum ipsam laborum deleniti </span><strong>velit
-                                    pariatur architecto aut nihil</strong></h3>
-                            <p class="">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                            </p>
-                        </div>
-
-                        <div class="faq-container px-xl-5" data-aos="fade-up" data-aos-delay="200">
-
-                            <div class="faq-item faq-active">
-
-                                <h3><span>01</span> Non consectetur a erat nam at lectus urna duis?</h3>
-                                <div class="faq-content">
-                                    <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus
-                                        laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor
-                                        rhoncus dolor purus non.</p>
-                                </div>
-                                <i class="faq-toggle bi bi-chevron-right"></i>
-                            </div><!-- End Faq item-->
-
-                            <div class="faq-item">
-                                <h3><span>02</span> Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?
-                                </h3>
-                                <div class="faq-content">
-                                    <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id
-                                        interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus
-                                        scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim.
-                                        Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
-                                </div>
-                                <i class="faq-toggle bi bi-chevron-right"></i>
-                            </div><!-- End Faq item-->
-
-                            <div class="faq-item">
-                                <h3><span>03</span> Dolor sit amet consectetur adipiscing elit pellentesque?</h3>
-                                <div class="faq-content">
-                                    <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci.
-                                        Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl
-                                        suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis
-                                        convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-                                    </p>
-                                </div>
-                                <i class="faq-toggle bi bi-chevron-right"></i>
-                            </div><!-- End Faq item-->
-
-                        </div>
-
-                    </div>
-
-                    <div class="col-lg-5 order-1 order-lg-2 why-us-img">
-                        <img src="http://localhost/BootstrapMade/Builder/components/img/template/Arsha/why-us.png"
-                            class="img-fluid" alt="" data-aos="zoom-in" data-aos-delay="100">
-                    </div>
-                </div>
-
-            </div>
-
-        </section><!-- /Why Us Section -->
-
-        <!-- Skills Section -->
-        <section id="skills" class="skills section">
-
-            <div class="container" data-aos="fade-up" data-aos-delay="100">
-
-                <div class="row">
-
-                    <div class="col-lg-6 d-flex align-items-center">
-                        <img src="assets/img/skills.png" class="img-fluid" alt="">
-                    </div>
-
-                    <div class="col-lg-6 pt-4 pt-lg-0 content">
-
-                        <h3>Voluptatem dignissimos provident quasi corporis voluptas</h3>
-                        <p class="fst-italic">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore et dolore magna aliqua.
-                        </p>
-
-                        <div class="skills-content skills-animation">
-
-                            <div class="progress">
-                                <span class="skill"><span>HTML</span> <i class="val">100%</i></span>
-                                <div class="progress-bar-wrap">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="100"
-                                        aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div><!-- End Skills Item -->
-
-                            <div class="progress">
-                                <span class="skill"><span>CSS</span> <i class="val">90%</i></span>
-                                <div class="progress-bar-wrap">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="90"
-                                        aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div><!-- End Skills Item -->
-
-                            <div class="progress">
-                                <span class="skill"><span>JavaScript</span> <i class="val">75%</i></span>
-                                <div class="progress-bar-wrap">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="75"
-                                        aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div><!-- End Skills Item -->
-
-                            <div class="progress">
-                                <span class="skill"><span>Photoshop</span> <i class="val">55%</i></span>
-                                <div class="progress-bar-wrap">
-                                    <div class="progress-bar" role="progressbar" aria-valuenow="55"
-                                        aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div><!-- End Skills Item -->
-
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-
-        </section><!-- /Skills Section -->
-
         <!-- Services Section -->
         <section id="services" class="services section">
-
             <!-- Section Title -->
             <div class="container section-title" data-aos="fade-up">
-                <h2>Services</h2>
-                <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+                <h2>Layanan</h2>
+                <p>Di Sini kami melayani kebutuhan mencuci dan penatu anda</p>
             </div><!-- End Section Title -->
-
             <div class="container">
-
                 <div class="row gy-4">
-
                     <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
                         <div class="service-item position-relative">
                             <div class="icon"><i class="bi bi-activity icon"></i></div>
-                            <h4><a href="" class="stretched-link">Lorem Ipsum</a></h4>
-                            <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+                            <h4><a href="" class="stretched-link">Cuci Kering Express</a></h4>
+                            <p>Cocok untuk pakaian yang perlu segera dipakai, seperti kemeja, celana bahan, atau baju pesta.</p>
                         </div>
                     </div><!-- End Service Item -->
 
